9ed4c8d5993557fef6be5b5af2ec075a2d8b5493
MychaelJales <mychael_duarte@hotmail.com>
2022-05-12 16:57:32 -0300

    :build: adiciona o cors a aplicacao

--- teste-backend/src/services/orderService.js
+++ teste-backend/src/services/orderService.js
@@ -3,9 +3,18 @@ const { order, provider, buyer, cnpj } = require('../models');
 const getAll = async () => {
   try {
     const orders = await order.findAll({
-      include: [
-        { model: provider, as: 'providers', include: [{ model: cnpj, as: 'cnpj' }] },
-        { model: buyer, as: 'buyers' },
+      attributes: ['nNf', 'emissionDate', 'value', 'orderStatusBuyer'],
+      include: [{
+          model: provider,
+          as: 'providers',
+          attributes: ['name', 'tradingName'],
+          include: [{ model: cnpj, as: 'cnpj', attributes: ['cnpj'] }],
+        },
+        {
+          model: buyer,
+          as: 'buyers',
+          attributes: ['name', 'tradingName'],
+        },
       ],
     });
 
