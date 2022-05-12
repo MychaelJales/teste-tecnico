3dd76f1e41b1403dd27e583827f15a16cb9048b8
MychaelJales <mychael_duarte@hotmail.com>
2022-05-12 20:02:52 -0300

    feat: criando componente myInvoices

---
+++ teste-frontend/src/components/MyInvoices.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <div class="top-bar"></div>
+    <div class="container-body">
+      <div class="header-container">
+        <div>
+          <img class="header-icon" src="../assets/header-icon.svg" alt="icone do menu">
+          <p class="header-text">Notas fiscais</p>
+        </div>
+        <p class="text-subtitle">Visualize as notas fiscais que você tem.</p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>NOTA FISCAL</th>
+            <th>SACADO</th>
+            <th>CEDENTE</th>
+            <th>EMISSÃO</th>
+            <th>VALOR</th>
+            <th>STATUS</th>
+            <th></th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyInvoices',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+.container {
+  width: 81vw;
+  background: #FFFFFF;
+  box-shadow: 0px 10px 30px rgba(225, 229, 236, 0.5);
+  border-radius: 15px 0px 0px 15px;
+}
+
+.top-bar {
+  height: 7vh;
+  border-bottom: 1px solid #DFE2EB;
+}
+
+.header-container div{
+  display: flex;
+
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: left;
+}
+
+.header-container {
+  height: 20px;
+  padding: 20px;
+}
+
+.header-text {
+ font-style: normal;
+  font-weight: 700;
+  color: #021B51;
+  font-size: 12px;
+  padding-right: 4px;
+}
+
+.header-icon {
+  height: 10px;
+  padding-right: 4px;
+}
+
+.text-subtitle {
+  padding-top: 5px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 8px;
+  color: #727D94;
+}
+
+table {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+th {
+  font-size: 8px;
+  font-style: normal;
+  font-weight: 500;
+  color: #A1A8B8;
+  padding-left: 10px;
+  padding-right: 40px;
+}
+</style>
--- teste-frontend/src/pages/MyInvoicesPage.vue
+++ teste-frontend/src/pages/MyInvoicesPage.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
+  <div class="container-page">
     <my-side-bar />
+    <my-invoices />
   </div>
 </template>
 
 <script>
 import MySideBar from '@/components/MySideBar.vue';
+import MyInvoices from '@/components/MyInvoices.vue';
 
 export default {
   name: 'MyInvoicesPage',
   components: {
     MySideBar,
+    MyInvoices,
   },
   data() {
     return {
@@ -19,4 +22,8 @@ export default {
 }
 </script>
 <style>
+.container-page {
+  display: flex;
+  flex-direction: row;
+}
 </style>
